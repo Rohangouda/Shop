e2de29646a153fb96e13ef73ad3c697abb9f6cc6
ROHAN KUMAR GOUDA <88418631+Rohangouda@users.noreply.github.com>
2024-04-16 07:46:51 +0000

    link added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -58,6 +58,16 @@ const Footer = () => {
                   </a>
                 </div>
               </div>
+              <a className="fixed top-4 right-6" href="https://www.buymeacoffee.com/rohangouda" style={{"zIndex" : 51}} target="_blank">
+                <button className="rounded-md px-4 py-2 flex" style={{backgroundColor: "rgb(224, 114, 104)", color: "rgb(0, 0, 0)"}}>
+                  <div className="flex">
+                      <span className="btn-icon px-1">
+                        <img src={Chai} alt="Chai icon" />
+                      </span>
+                      <span className="btn-custom-text">Buy us a chai</span>
+                </div>
+                </button>
+                </a>
             </div>
             <div className="footer-contact">
               <h3>Call Us</h3>
